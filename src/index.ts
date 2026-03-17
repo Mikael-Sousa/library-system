2c299beb822381fb3995fbb6224d6de07c9c1098
Mikael-Sousa <mikael.sr.dev@gmail.com>
2026-03-17 15:23:43 -0300

    primeiro commit

---
+++ src/index.ts
@@ -0,0 +1,82 @@
+let userId = 1
+let bookid = 1
+
+class Book {
+    static books: Book[] = []
+
+    id: number;
+    name: string;
+    author: string;
+    available: boolean
+
+    constructor(
+        name: string,
+        author: string) {
+        this.id = bookid++
+        this.name = name
+        this.author = author
+        this.available = true
+
+        Book.books.push(this)
+    }
+    borrow() {
+        if (this.available) {
+            this.available = false
+        }
+        else {
+            console.log("This book is not available")
+        }
+    }
+    giveBack() {
+        if (!this.available) {
+            this.available = true
+        }
+        else {
+            console.log("This book is not borrowed")
+        }
+    }
+}
+
+class User {
+    static users: User[] = [];
+    
+    id: number;
+    name: string;
+    borrowedBooks: Book[] = [];
+
+    constructor(
+        name: string) {
+        this.id = userId++
+        this.name = name
+
+        User.users.push(this)
+    }
+
+    consultCatalog() {
+        console.log(Book.books)
+    }
+
+    borrow(book: Book) {
+        book.borrow()
+        this.borrowedBooks.push(book)
+    }
+    giveBack(book: Book) {
+        book.giveBack()
+    }
+}
+
+const book1 = new Book("Jorge A.", "vidas secas")
+const book2 = new Book("Jorge A.", "vidas secas")
+const book3 = new Book("Jorge A.", "vidas secas")
+
+const user1 = new User("gabriel")
+const user2 = new User("gabriela")
+
+user1.borrow(book1)
+user1.borrow(book2)
+user1.borrow(book3)
+
+user1.consultCatalog()
+console.log(User.users)
+console.log(user1.borrowedBooks)
+
